2c2e73cf6254e2a81613d52d10352041573ff8f2
Mariya <grmarianna5@gmail.com>
2022-11-08 19:04:14 +0300

    Add new files.

--- lab2_1/main.c
+++ lab2_1/main.c
@@ -2,10 +2,29 @@
 #include <stdlib.h>
 void main()
 {
-	int a,n;
+	int b1, b2, b3, b4, b5, b6, n;
 	printf("Enter a six-digit number: ");
 	scanf("%d", &n);
-	a=n/10;
-	printf("answer %d", a);
+	
+	b1=n%10;
+	n=n/10;
+	
+	b2=n%10;
+	n=n/10;
+	
+	b3=n%10;
+	n=n/10;
+	
+	b4=n%10;
+	n=n/10;
+	
+	b5=n%10;
+	n=n/10;
+	
+	b6=n%10;
+	n=n/10;
+	
+	if(b1+b2+b3==b4+b5+b6) printf("The ticket is lucky");
+	 else printf("The ticket is not lucky");
 }
 
